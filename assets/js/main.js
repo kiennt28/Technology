c47ad1e9048f3030c5312244641a1755c1d9081d
kiennt28 <trungkien28.dev@gmail.com>
2021-11-27 17:43:19 +0700

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,30 @@
-window.onscroll = function () {
-  scrollFunction();
-};
-var navbar = document.querySelector("header");
-var sticky = navbar.offsetTop;
+// window.onscroll = function () {
+//   scrollFunction();
+// };
+// var navbar = document.querySelector("header");
+// var sticky = navbar.offsetTop;
 
-function scrollFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+// function scrollFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+const x = document.querySelector.bind(document);
+const y = document.querySelectorAll.bind(document);
+
+const tabs = y(".tab");
+const panes = y(".timetab-container");
+
+tabs.forEach(function (tab, index) {
+  const pane = panes[index];
+  tab.onclick = function () {
+    console.log(pane);
+    x(".tab.active").classList.remove("active");
+    x(".timetab-container.active").classList.remove("active");
+    this.classList.add("active");
+    pane.classList.add("active");
+  };
+});
