51267fc99adabf07e74723827f4f4cfb3a6a2855
kiennt28 <trungkien28.dev@gmail.com>
2021-11-28 13:55:32 +0700

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,13 +18,39 @@ const y = document.querySelectorAll.bind(document);
 const tabs = y(".tab");
 const panes = y(".timetab-container");
 
+const tabAddress = y(".nav-link");
+const tabMaps = y(".tab-pane");
+
+const tabActive = x(".nav-link.active");
+const line = x(".nav-tabs .line");
+// console.log(line);
+
+line.style.left = tabActive.offsetLeft + "px";
+line.style.width = tabActive.offsetWidth + "px";
+
+// Areas of strength-Active
 tabs.forEach(function (tab, index) {
   const pane = panes[index];
   tab.onclick = function () {
-    console.log(pane);
     x(".tab.active").classList.remove("active");
     x(".timetab-container.active").classList.remove("active");
+
     this.classList.add("active");
     pane.classList.add("active");
   };
 });
+
+// Contact-Active 
+tabAddress.forEach(function (tab, index) {
+  const tabMap = tabMaps[index];
+  tab.onclick = function () {
+    x(".nav-link.active").classList.remove("active");
+    x(".tab-pane.active").classList.remove("active");
+
+    line.style.left = this.offsetLeft + "px";
+    line.style.width = this.offsetWidth + "px";
+
+    this.classList.add("active");
+    tabMap.classList.add("active");
+  }
+})
