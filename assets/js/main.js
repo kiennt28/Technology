9816470f10fe6522256fca8fdef9bc8031dc0ca6
kiennt28 <trungkien28.dev@gmail.com>
2021-11-29 11:00:13 +0700

    s

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,29 @@
-// window.onscroll = function () {
-//   scrollFunction();
-// };
-// var navbar = document.querySelector("header");
-// var sticky = navbar.offsetTop;
+// Slider
+var slideIndex = 0;
+showSlides();
 
-// function scrollFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky");
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
+function showSlides() {
+  var slides = document.getElementsByClassName("mySlider");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
 
-const x = document.querySelector.bind(document);
-const y = document.querySelectorAll.bind(document);
+  setTimeout(showSlides, 2000);
+}
 
-const tabs = y(".tab");
-const panes = y(".timetab-container");
+const tabs = document.querySelectorAll(".tab");
+const panes = document.querySelectorAll(".timetab-container");
 
-const tabAddress = y(".nav-link");
-const tabMaps = y(".tab-pane");
+const tabAddress = document.querySelectorAll(".nav-link");
+const tabMaps = document.querySelectorAll(".tab-pane");
 
-const tabActive = x(".nav-link.active");
-const line = x(".nav-tabs .line");
-// console.log(line);
+const tabActive = document.querySelector(".nav-link.active");
+const line = document.querySelector(".nav-tabs .line");
 
 line.style.left = tabActive.offsetLeft + "px";
 line.style.width = tabActive.offsetWidth + "px";
@@ -32,25 +32,27 @@ line.style.width = tabActive.offsetWidth + "px";
 tabs.forEach(function (tab, index) {
   const pane = panes[index];
   tab.onclick = function () {
-    x(".tab.active").classList.remove("active");
-    x(".timetab-container.active").classList.remove("active");
+    document.querySelector(".tab.active").classList.remove("active");
+    document
+      .querySelector(".timetab-container.active")
+      .classList.remove("active");
 
     this.classList.add("active");
     pane.classList.add("active");
   };
 });
 
-// Contact-Active 
+// Contact-Active
 tabAddress.forEach(function (tab, index) {
   const tabMap = tabMaps[index];
   tab.onclick = function () {
-    x(".nav-link.active").classList.remove("active");
-    x(".tab-pane.active").classList.remove("active");
+    document.querySelector(".nav-link.active").classList.remove("active");
+    document.querySelector(".tab-pane.active").classList.remove("active");
 
     line.style.left = this.offsetLeft + "px";
     line.style.width = this.offsetWidth + "px";
 
     this.classList.add("active");
     tabMap.classList.add("active");
-  }
-})
+  };
+});
